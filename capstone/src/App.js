dd9207df929b9d0327a55eaaad674244910166df
Tay <traig941@gmail.com>
2023-09-17 23:41:57 -0700

    base updates

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App() = () => {
+
+  const [categories, setCatergories] = useState([hats, jackets, sneakers, womens, mens]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='categories-container'>
+      {categories.map(category => {
+        return (
+          <div className='category-container'>
+            <h2>{category}</h2>
+            <p>Shop Now</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
